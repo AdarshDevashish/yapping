9d5333521238745c723797297d750e4c13469fe9
Adarsh Devashish <devashishadarsh@gmail.com>
2024-06-28 21:04:27 +0530

    added providers and login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Button from "@/components/ui/Button"
-import { db } from "@/lib/db"
+import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
 export default async function Home() {
-  return <Button>Hello Bro</Button>
+  return <Button>Hello Bro</Button>;
 }
