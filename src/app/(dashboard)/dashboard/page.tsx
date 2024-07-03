c707ce3a1594ca2f1fb09eaea5d143eda56d4161
Adarsh Devashish <devashishadarsh@gmail.com>
2024-07-03 16:29:37 +0530

    Created add friend functionality

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 
 const page = async ({}) => {
   const session = await getServerSession(authOptions);
-  return <pre>{JSON.stringify(session)}</pre>;
+  return <pre>{JSON.stringify(session, null, 2)}</pre>
 }
 
 export default page
